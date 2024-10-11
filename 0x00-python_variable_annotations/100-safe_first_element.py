7f88de926c519668d4431a1d6852a8dfe6c8118a
KKirimi <keithkirimi.kk@gmail.com>
2024-10-11 13:05:55 +0000

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,7 @@
 
 from typing import Sequence, Any, Union
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     ''' Return the first element of lst if there is any, otherwise None. '''
     if lst:
